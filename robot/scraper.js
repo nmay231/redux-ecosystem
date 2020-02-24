ba7bec60c210cccfb2f757480f1ad73010168166
Noah May <noahmouse2011@gmail.com>
2020-02-24 13:10:34 -0700

    Fixed parsing bugs

--- robot/scraper.js
+++ robot/scraper.js
@@ -5,12 +5,12 @@ const { execSync } = require('child_process')
 const fs = require('fs')
 const path = require('path')
 const ora = require('ora')
+const sequelize = require('sequelize')
 
 const { get: getNPMStats } = require('download-stats')
 const { parse: parseMarkdown } = require('remark')
 
 const { githubAPIKey } = require('../utils/consts')
-const databaseLocation = path.resolve(__dirname, '../public/database.json')
 const errorsLocation = path.resolve(__dirname, 'errors.json')
 const repoLocation = path.resolve(__dirname, 'links')
 const fileBlacklist = ['.git', '.github', 'README.md']
@@ -19,7 +19,8 @@ const slugify = (str) =>
     str
         .toLowerCase()
         .replace(/( ?- ?)|\/| /g, '-')
-        .replace(/\(|\)/g, '')
+        .replace(/[^-\w]/g, '')
+
 const fetchJSON = async (uri) =>
     fetch(uri, { headers: { Authorization: 'token ' + githubAPIKey } }).then((r) => r.json())
 
@@ -48,7 +49,7 @@ execSync(`git --git-dir=${gitDir} log --oneline -n 1`, { stdio: 'inherit' })
 
 const progress = ora('Reading files...').start()
 const final = []
-const errors = []
+const parsingErrors = []
 
 for (let fileName of fs.readdirSync(repoLocation)) {
     if (fileBlacklist.indexOf(fileName) > -1) continue
@@ -72,7 +73,7 @@ for (let fileName of fs.readdirSync(repoLocation)) {
     for (let child of ast.children) {
         if (child.type === 'heading' && child.depth === 3) {
             if (category.name) {
-                errors.push({
+                parsingErrors.push({
                     fileName,
                     subcategoryName: subcategory.name,
                     message: `Error: duplicate category header found "${child.children[0].value}". Aborting file!`,
@@ -90,7 +91,7 @@ for (let fileName of fs.readdirSync(repoLocation)) {
 
         if (child.type === 'heading' && child.depth === 4) {
             if (!category) {
-                errors.push({
+                parsingErrors.push({
                     fileName,
                     subcategoryName: subcategory.name,
                     message: `Error: found subcategory without category header "${subcategory}". Aborting file!`,
@@ -110,7 +111,7 @@ for (let fileName of fs.readdirSync(repoLocation)) {
             // If the first paragraph does not have a title, link, and description (and 2 breaks), then skip.
             const firstParagraph = child.children[0].children[0]
             if (firstParagraph.children.length < 5) {
-                errors.push({
+                parsingErrors.push({
                     fileName,
                     subcategoryName: subcategory.name,
                     message: 'Warning: skipping list in category because not a list of repos',
@@ -121,7 +122,7 @@ for (let fileName of fs.readdirSync(repoLocation)) {
 
             // Check for single header files (with no subcategories) like routing.md
             if (!subcategory.name) {
-                errors.push({
+                parsingErrors.push({
                     fileName,
                     subcategoryName: subcategory.name,
                     message: 'Warning: assuming this file has no subcategories',
@@ -182,8 +183,8 @@ const totalCount = final.reduce(
     0,
 )
 progress.succeed(
-    `Parsed files with ${errors.length} warnings.
-    Found ${totalCount} total repositories/projects.`,
+    `Parsed files with ${parsingErrors.length} warnings.
+  Found ${totalCount} total repositories/projects.`,
 )
 
 const makeGithubQuery = (owner, name) => `
@@ -204,13 +205,13 @@ const makeGithubQuery = (owner, name) => `
 `
 
 const batchGithubRequest = async (projects) => {
-    const queries = projects.map((project) => {
+    const queries = projects.map((project, i) => {
         if (!project.githubURL) return ''
         const [, , , repoOwner, repoName, ...stuff] = project.githubURL.split('/')
-        return makeGithubQuery(repoOwner, repoName)
+        return `repo${i}: ` + makeGithubQuery(repoOwner, repoName)
     })
+    const masterQuery = `query { ${queries.join(' ')} }`
 
-    const masterQuery = `query { ${queries.map((q, i) => `repo${i}: ${q}`).join(' ')} }`
     const r = await fetch('https://api.github.com/graphql', {
         method: 'POST',
         headers: {
@@ -230,7 +231,7 @@ const batchGithubRequest = async (projects) => {
             name: repo.name || project.name,
             description: project.description,
             githubURL: repo.url || project.url,
-            altURLs: (progress.altURLs || []).join(';;;'),
+            altURLs: project.altURLs || [],
             githubStars: repo.stargazers && repo.stargazers.totalCount,
             lastRelease,
             npmDownloadsThisMonth: project.npmDownloadsThisMonth,
@@ -240,7 +241,10 @@ const batchGithubRequest = async (projects) => {
     return { projects, errors }
 }
 
-main()
+main().catch((err) => {
+    console.error(err)
+    process.exit(1)
+})
 
 async function main() {
     let asyncErrors = []
@@ -260,12 +264,13 @@ async function main() {
             const subcategory = category.subcategories[index]
 
             progress.text =
-                `(${currentCategory - -1}/${catCount}) ${category.slug} -` +
-                ` ${index - -1}/${length} ${subcategory.name}`
+                `(${parseInt(currentCategory) + 1}/${catCount}) ${category.slug} -` +
+                ` ${parseInt(index) + 1}/${length} ${subcategory.slug}`
             try {
                 const { projects, errors } = await batchGithubRequest(subcategory.repositories)
 
-                asyncErrors = asyncErrors.concat(errors || [])
+                if (errors)
+                    asyncErrors.push({ where: `${category.slug}/${subcategory.slug}`, errors })
 
                 subcategory.repositories = await Promise.all(
                     projects.map(async (project) => {
@@ -290,18 +295,87 @@ async function main() {
         }
 
         progress.succeed(
-            `(${currentCategory - -1}) ${category.slug} completed: ${totalCount}/${totalCount} projects`,
+            `(${parseInt(currentCategory) + 1}) ${category.slug}` +
+                ` completed: ${totalCount}/${totalCount} projects`,
         )
     }
 
-    if (fs.existsSync(databaseLocation)) fs.truncateSync(databaseLocation, 0)
-    fs.writeFileSync(
-        databaseLocation,
-        JSON.stringify({ categories: final, last_updated: Date() }, null, 2),
-    )
+    const databaseLocation = path.resolve(__dirname, '../database.sqlite')
+    const progress = ora('Creating database...').start()
 
-    if (fs.existsSync(errorsLocation)) fs.truncateSync(errorsLocation, 0)
-    fs.writeFileSync(errorsLocation, JSON.stringify({ errors, asyncErrors }, null, 2))
-    console.log(`${asyncErrors.length} errors fetching data`)
+    const db = new sequelize.Sequelize({
+        dialect: 'sqlite',
+        storage: databaseLocation,
+        logging: false,
+    })
+
+    const Category = db.define('category', {
+        name: sequelize.TEXT,
+        slug: sequelize.TEXT,
+    })
+
+    const Subcategory = db.define('subcategory', {
+        name: sequelize.TEXT,
+        slug: sequelize.TEXT,
+        categoryId: {
+            type: sequelize.INTEGER,
+            references: {
+                model: Category,
+                key: 'id',
+            },
+        },
+    })
+
+    const Project = db.define('project', {
+        name: sequelize.TEXT,
+        description: sequelize.TEXT,
+        githubURL: sequelize.TEXT,
+        altURLs: sequelize.TEXT,
+        githubStars: sequelize.INTEGER,
+        githubLastUpdate: sequelize.DATEONLY,
+        githubLastPush: sequelize.DATEONLY,
+        npmDownloadsThisMonth: sequelize.INTEGER,
+        categoryId: {
+            type: sequelize.INTEGER,
+            references: {
+                model: Subcategory,
+                key: 'id',
+            },
+        },
+    })
+
+    // Create new tables
+    await db.sync({ force: true })
+
+    for (let category of final) {
+        Category.create(category)
+        Category.sync()
+        const { id: categoryId } = await Category.findOne({
+            where: { slug: category.slug },
+            attributes: ['id'],
+        })
+        for (let subcategory of category.subcategories) {
+            Subcategory.create({ ...subcategory, categoryId })
+            Subcategory.sync()
+            const { id: subcategoryId } = await Subcategory.findOne({
+                where: { slug: subcategory.slug },
+                attributes: ['id'],
+            })
+            for (let project of subcategory.repositories) {
+                const altURLs =
+                    typeof project.altURLs === 'string'
+                        ? project.altURLs
+                        : project.altURLs.join(';;;')
+                await Project.create({ ...project, altURLs, subcategoryId })
+            }
+            Project.sync()
+        }
+    }
+
+    if (fs.existsSync(errorsLocation)) fs.truncateSync(errorsLocation)
+    fs.writeFileSync(errorsLocation, JSON.stringify({ parsingErrors, asyncErrors }, null, 2))
+
+    db.sync()
+    progress.succeed(`Data saved to file "${databaseLocation}"`)
     console.log(`Saved data to file: ${databaseLocation}`)
 }
